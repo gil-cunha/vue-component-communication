f52ad679eda2940bbf32e5a7b90c100b393a8fe5
Gil Cunha <gilcunha@outlook.pt>
2022-11-21 17:46:54 +0000

    passagem de props

--- assets/vue-scripts.js
+++ assets/vue-scripts.js
@@ -1,4 +1,23 @@
+// Criação do componente
 Vue.component('blog-post', {
     props: ['post'],
-    template: 
+    template: `
+        <div>
+            <h3>{{ post.title }}</h3>
+            <div v-html="post.content"></div>
+        </div>
+    `
 })
+
+// Instãncia Vue
+new Vue({
+    el: '#my-blog-post-demo',
+    data: {
+        posts: [
+            { id: 1, title: 'Introdução ao Vue', content: '<p>Texto de introdução ao Vue.</p>' },
+            { id: 2, title: 'Criação de instãncias', content: '<p>Texto de criação de instãncias.</p>' },
+            { id: 3, title: 'Ciclo de vida das instãncias', content: '<p>Texto do ciclo de vida das instãncias.</p>' }
+        ]
+    }
+})
+
