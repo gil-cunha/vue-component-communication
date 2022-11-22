6ac36d1bb81fa5cc6fcff76685431bfbaaa2c228
Gil Cunha <gilcunha@outlook.pt>
2022-11-22 06:00:26 +0000

    envio de props e emissão de eventos

--- assets/vue-scripts.js
+++ assets/vue-scripts.js
@@ -1,9 +1,12 @@
-// Criação do componente
+// Criação do componente (componente pai)
 Vue.component('blog-post', {
     props: ['post'],
     template: `
         <div>
             <h3>{{ post.title }}</h3>
+            <button v-on:click="$emit('enlarge-text', 0.1)">
+                Aumentar fonte
+            </button>
             <div v-html="post.content"></div>
         </div>
     `
@@ -17,7 +20,13 @@ new Vue({
             { id: 1, title: 'Introdução ao Vue', content: '<p>Texto de introdução ao Vue.</p>' },
             { id: 2, title: 'Criação de instãncias', content: '<p>Texto de criação de instãncias.</p>' },
             { id: 3, title: 'Ciclo de vida das instãncias', content: '<p>Texto do ciclo de vida das instãncias.</p>' }
-        ]
+        ],
+        postFontSize: 1
+    },
+    methods:{
+        onEnlargeText: function(enlargeAmount){
+            this.postFontSize += enlargeAmount
+        }
     }
 })
 
